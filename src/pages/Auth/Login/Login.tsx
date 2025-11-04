61217f19b116cf93ca6fc363d26be2250d3bf201
OhSangH <dhtkdgus126@gmail.com>
2025-11-04 17:42:07 +0900

    feat : 20251104 login component

--- src/pages/Auth/Login/Login.tsx
+++ src/pages/Auth/Login/Login.tsx
@@ -1,8 +1,20 @@
+import { ChangeEvent, ChangeEventHandler, useState } from 'react';
 
+type User = {
+  email: string;
+  password: string;
+};
 function Login() {
-  return (
-    <div>Login(회원가입은 useNavigate 처리 고려)</div>
-  )
+  const [user, setUser] = useState<User>({
+    email: '',
+    password: '',
+  });
+
+  const [isAuthenticate, setIsAuthenticate] = useState(false);
+
+  const changeHandler = (event : ChangeEvent<HTMLInputElement>)
+
+  return <div>Login(회원가입은 useNavigate 처리 고려)</div>;
 }
 
-export default Login
+export default Login;
