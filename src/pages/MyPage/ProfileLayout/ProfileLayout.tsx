bb2f0173477fbf7b05c0238d65aa68a0ec5f2130
Lui-die-lui <lsg05h@naver.com>
2025-11-05 17:47:34 +0900

    feat:Mypage구현

--- src/pages/MyPage/ProfileLayout/ProfileLayout.tsx
+++ src/pages/MyPage/ProfileLayout/ProfileLayout.tsx
@@ -1,7 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import * as s from "./styles";
 
-// 프로필 화면 혹은 프로필 변경 화면 레이아
+// 프로필 화면 혹은 프로필 변경 화면 레이아웃
 function ProfileLayout({ children }) {
   return <div css={s.container}>{children}</div>;
 }
--- src/routes/MainRouter/MainRouter.tsx
+++ src/routes/MainRouter/MainRouter.tsx
@@ -4,19 +4,21 @@ import PerformanceRouter from "../PerformanceRouter/PerformanceRouter";
 import Home from "../../pages/Home/Home";
 import Theater from "../../pages/PerformancePlace/PerformancePlace";
 import Login from "../../pages/Auth/Login/Login";
-import MyPage from "../../pages/MyPage/MyPage";
 
 function MainRouter() {
   return (
     <Routes>
       <Route element={<Layout />}>
-        {/* index 사옹시 기본 경로와 동일합니다. */}
+        {/* index 사용시 기본 경로와 동일합니다. */}
         <Route index element={<Home />} />
         {/* 로그인 엔드포인트 */}
-        <Route path="/login" element={
-          // <Login />
-          <MyPage />
-          } />
+        <Route
+          path="/login"
+          element={
+            <Login />
+            // <MyPage />
+          }
+        />
         {/* 퍼포먼스 라우터에서 페이지 전환이 있기 때문에 개별적으로 라우터 만들었습니다. 작업하시는 분이  라우터 대신 useNavigate쓰는게 옳다고 생각할 경우 usenavigate 써도 상관 없습니다.*/}
         <Route path="/performance/*" element={<PerformanceRouter />} />
         {/* 공연장 */}
