dd1127f7c496e763f56db22b0d69ccc4bc345d49
Lui-die-lui <lsg05h@naver.com>
2025-11-10 17:13:22 +0900

    feat:20251110 principalState & MyPage

--- src/stores/usePrincipalState.ts
+++ src/stores/usePrincipalState.ts
@@ -21,25 +21,7 @@ interface PrincipalState {
 }
 
 export const usePrincipalState = 
-// create<PrincipalState>((set) => ({
-//   isAuthenticated: false,
-//   principal: null,
 
-//   login: (userData) => set({ isAuthenticated: true, principal: userData }),
-
-//   logout: () => {
-//     // jwt 토큰 삭제
-//     localStorage.removeItem("jwt");
-//     // 캐시 초기화
-//     queryClient.clear();
-//     // 로그아웃 상태로 만들어줌
-//     set({ isAuthenticated: false, principal: null });
-//     // 로그인 페이지로 리다이렉트
-//     window.location.href = "/login";
-//   },
-// }),
-
-// );
 create(
    persist<PrincipalState>(
     (set) => ({
@@ -56,57 +38,7 @@ create(
       },
     }),
     {
-      name: "principal-storage", // localStorage에 저장될 key 이름 ... 이거 해야함? 꼭?
+      name: "principal-storage", // localStorage에 저장됨 - 이거 있으면 로그아웃 하기 전까지 로그아웃 x
     }
   )
 )
-// import { queryClient } from "./../configs/queryClient";
-// import { create } from "zustand";
-// import { persist } from "zustand/middleware";
-
-// // 로그인 유저 데이터 인터페이스
-// interface Principal {
-//   id: number;
-//   username: string;
-//   email: string;
-//   profileImg?: string;
-//   role: string;
-// }
-
-// interface PrincipalState {
-//   isAuthenticated: boolean;
-//   principal: Principal | null;
-//   login: (userData: Principal, token: string) => void;
-//   logout: () => void;
-// }
-
-// export const usePrincipalState = create(
-//   persist<PrincipalState>(
-//     (set) => ({
-//       isAuthenticated: false,
-//       principal: null,
-
-//       // ✅ 로그인 시
-//       login: (userData, token) => {
-//         sessionStorage.setItem("jwt", token);
-//         set({ isAuthenticated: true, principal: userData });
-//       },
-
-//       // ✅ 로그아웃 시
-//       logout: () => {
-//         sessionStorage.removeItem("jwt");
-//         queryClient.clear();
-//         set({ isAuthenticated: false, principal: null });
-//         window.location.href = "/login";
-//       },
-//     }),
-//     {
-//       name: "principal-storage", // 저장 key 이름
-//      storage: {
-//   getItem: (key) => sessionStorage.getItem(key),
-//   setItem: (key, value) => sessionStorage.setItem(key, JSON.stringify(value)),
-//   removeItem: (key) => sessionStorage.removeItem(key),
-// },
-//     }
-//   )
-// );
