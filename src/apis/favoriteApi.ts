319ae095488658fbf9d67422c6fb36a442f3bd35
Lui-die-lui <lsg05h@naver.com>
2025-11-11 17:09:50 +0900

    feat: favoriteApi updated

--- src/apis/favoriteApi.ts
+++ src/apis/favoriteApi.ts
@@ -1,6 +1,24 @@
 // 스크랩(좋아요) 관련 api
 
 import { instance } from "./instance";
+import axios from "axios";
+
+export type ScrapRank = {
+  prfName: string;
+  scrapCount: number;
+};
+
+export const getTop10Favorites = async (): Promise<ScrapRank[]> => {
+  const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+  try {
+    const response = await axios.get(`${BASE_URL}/favorite/top10`);
+    return response.data;
+  } catch (error) {
+    console.error("Failed to fetch top10 favorites", error);
+    throw error;
+  }
+};
 
 // 공연 스크랩 토글
 export const toggleFavoriteReq = async (prfId: string) => {
