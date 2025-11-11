eddd5a59f765551d3fdfa755f5ba9b57f9c6276c
OhSangH <dhtkdgus126@gmail.com>
2025-11-11 17:13:04 +0900

    feat : 20251111 Google button add

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,15 @@ import App from './App.tsx';
 import './index.css';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
+const googleClientId = `${import.meta.env.VITE_GOOGLE_CLIENT_ID}`;
+
+if (!googleClientId) {
+  console.warn('Google Client ID가 설정되지 않았습니다.');
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId=''>
+    <GoogleOAuthProvider clientId={googleClientId}>
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>
