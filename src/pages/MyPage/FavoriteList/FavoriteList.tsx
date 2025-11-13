4838f79e2d6e70afeb9e6e51a1d88314e935e82f
Lui-die-lui <lsg05h@naver.com>
2025-11-13 12:07:08 +0900

    feat: myPage favorite 개선

--- src/pages/MyPage/FavoriteList/FavoriteList.tsx
+++ src/pages/MyPage/FavoriteList/FavoriteList.tsx
@@ -4,19 +4,13 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getFavoritePrfListReq } from "../../../apis/favoriteApi";
 import PerformanceModal from "../../../components/common/PerformanceModal/PerformanceModal";
+import { useFavoriteState } from "../../../stores/useFavoriteState";
 
-type Performance = {
-  prfId: string;
-  posterImgUrl: string;
-  prfNm: string;
-  prfPlcNm: string;
-  prfStartDt: string;
-  prfEndDt: string;
-  genreNm: string;
-};
+
 
 function FavoriteList() {
-  const [favorites, setFavorites] = useState<Performance[]>([]);
+  // const [favorites, setFavorites] = useState<Performance[]>([]);
+  const {favoriteList, setFavoriteList} = useFavoriteState();
   const [open, setOpen] = useState(false);
    const [selectedPrfId, setSelectedPrfId] = useState<string | null>(null);
 
@@ -24,13 +18,14 @@ function FavoriteList() {
     const fetchFavorites = async () => {
       try {
         const data = await getFavoritePrfListReq();
-        setFavorites(data);
+        setFavoriteList(data);
       } catch (error) {
         console.error("스크랩 리스트 불러오기 실패 : ", error);
       }
     };
     fetchFavorites();
-  }, []);
+    // 의존성 배열 추가 - favoriteList 상태를 바로 반영
+  }, [setFavoriteList]);
 
   const openModalHandler = (prfId: string) => {
     setSelectedPrfId(prfId);
@@ -41,12 +36,15 @@ function FavoriteList() {
     <>
     <div css={s.container}>
       <h2 css={s.title}>❤️My Favorites</h2>
-      {favorites.length === 0 ? (
+      {favoriteList.length === 0 ? (
         <p css={s.empty}>아직 스크랩한 공연이 없습니다.</p>
       ) : (
         <ul css={s.list}>
-          {favorites.map((item) => (
-            <li key={item.prfId} css={s.card} onClick={() => openModalHandler(item.prfId)}>
+          {favoriteList.map((item) => (
+            <li 
+            key={item.prfId} 
+            css={s.card} 
+            onClick={() => openModalHandler(item.prfId)}>
               <img src={item.posterImgUrl} alt={item.prfNm} css={s.poster} />
 
               <div css={s.info}>
