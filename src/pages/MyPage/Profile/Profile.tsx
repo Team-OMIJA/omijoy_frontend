bf9194ef983f94ba0ecacdbe9526cab961c61a3c
Lui-die-lui <lsg05h@naver.com>
2025-11-18 09:37:29 +0900

    feat: 20251118 MyPage 프로필 편집 개선

--- src/pages/MyPage/Profile/Profile.tsx
+++ src/pages/MyPage/Profile/Profile.tsx
@@ -3,7 +3,7 @@ import ProfileLayout from "../ProfileLayout/ProfileLayout";
 import EditProfile from "../EditProfile/EditProfile";
 import ProfileView from "../ProfileView/ProfileView";
 import { usePrincipalState } from "../../../stores/usePrincipalState";
-import { useAuthState } from "../../../stores/useAuthState";
+
 
 function Profile() {
   const { principal } = usePrincipalState();
