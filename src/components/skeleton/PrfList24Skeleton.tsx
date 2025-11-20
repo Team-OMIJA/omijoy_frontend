981c98f809fcf881cf466359a7e7d92cbb3258ab
Lui-die-lui <lsg05h@naver.com>
2025-11-20 14:11:58 +0900

    feat: 20251120 skeleton ui edited

--- src/components/skeleton/PrfList24Skeleton.tsx
+++ src/components/skeleton/PrfList24Skeleton.tsx
@@ -1,19 +1,19 @@
 import PrfCardSkeleton from "./PrfCardSkeleton";
 
-export default function PrfList10Skeleton() {
+export default function PrfList24Skeleton() {
   return (
     <div
       style={{
         width: "1350px",
         margin: "0 auto",
         display: "grid",
-        gridTemplateColumns: "repeat(5, 1fr)",
+        gridTemplateColumns: "repeat(auto-fill, minmax(180px, 1fr))",
         columnGap: "30px",
         rowGap: "40px",
       }}
     >
-      {/* 카드 스켈레톤 5개 */}
-      {Array.from({ length: 20 }).map((_, i) => (
+      {/* 카드 스켈레톤 24개 */}
+      {Array.from({ length: 24 }).map((_, i) => (
         <PrfCardSkeleton key={i} />
       ))}
       {/*[undefined, undefined, undefined, undefined, undefined] 이런 배열 생성 -> .map()으로 PrfCardSkeleton을 5번 렌더링 */}
