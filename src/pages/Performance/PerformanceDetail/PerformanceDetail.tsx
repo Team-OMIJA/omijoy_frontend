4589875818efbd3e1709eefdff8c49671e759c85
silvervolt1234 <105256888+silvervolt1234@users.noreply.github.com>
2025-11-24 15:13:32 +0900

    refactor 20251124 detail fix

--- src/pages/Performance/PerformanceDetail/PerformanceDetail.tsx
+++ src/pages/Performance/PerformanceDetail/PerformanceDetail.tsx
@@ -33,6 +33,8 @@ function PerformanceDetail() {
   );
   const [loading, setLoading] = useState(true);
 
+  const [showLinks, setShowLinks] = useState(false);
+
   const { favorites, toggleFavorite, fetchFavoriteState } = useFavoriteState();
 
   const liked = id ? favorites[id] ?? false : false;
@@ -70,6 +72,30 @@ function PerformanceDetail() {
 
   const HeartIcon = liked ? ImHeart : SlHeart;
 
+  const getSiteName = (url: string) => {
+  const lower = url.toLowerCase();
+    if (lower.includes("interpark")) return "인터파크";
+    if (lower.includes("ticketlink")) return "티켓링크";
+    if (lower.includes("yes24")) return "YES24";
+    if (lower.includes("naver")) return "네이버 예매";
+    if (lower.includes("wemakeprice")) return "위메프";
+    if (lower.includes("melon")) return "멜론티켓";
+    if (lower.includes("lotte")) return "롯데콘서트홀";
+    if (lower.includes("coffee")) return "커넥티브 티켓";
+    if (lower.includes("nanumticket")) return "나눔 티켓";
+    try {
+      const hostname = new URL(url).hostname;
+      const parts = hostname.replace("www.", "").split(".");
+      const mainDomain = parts[0];
+
+      return mainDomain.charAt(0).toUpperCase() + mainDomain.slice(1);
+    } catch {
+      return "예매처";
+    }
+  };
+
+
+
   return (
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
       <div
@@ -80,10 +106,13 @@ function PerformanceDetail() {
           width: "75%",
           marginRight: "120px",
           marginTop: "25px",
-          alignItems: "center",
+          // alignItems: "center",
+          gap: "100px",
+          alignItems: "flex-start",
           position: "relative",
         }}
       >
+      <div>
         <img
           src={performance.posterImgUrl}
           alt={performance.prfNm}
@@ -91,87 +120,140 @@ function PerformanceDetail() {
           height={500}
           style={{ borderRadius: "10px" }}
         />
-        <HeartIcon style={{position: "absolute", top: "40px", right: "-80px", fontSize: "40px", color: "crimson", cursor: "pointer"}}  onClick={handleToggleFavorite}/>
       </div>
-      <s.PerformanceTitle>
-            <h1>{removeRegionTag(performance.prfNm)}</h1>
-          </s.PerformanceTitle>
-          <s.PerformanceDetail1>
-            <h2 style={{ margin: 0, lineHeight: 2 }}>
-              {(() => {
-                const original = performance.prfPlcNm;
-                let result = '';
-                const seen = new Set<string>();
-                original.split(/\s*(\([^)]+\))/).forEach((part) => {
-                  if (!part) return;
-                  if (!part.startsWith('(')) {
-                    result += part;
-                    return;
-                  }
-                  const content = part.slice(1, -1).trim();
-                  const normalizedContent = content.replace(/\s+/g, '');
-                  const normalizedResult = result.replace(/\s+/g, '');
-                  if (seen.has(normalizedContent) || normalizedResult.includes(normalizedContent)) {
-                    return;
-                  }
-                  seen.add(normalizedContent);
-                  result += `(${content})`;
-                });
-                return result.trim();
-              })()}
-            </h2>
-            <p>{performance.area}</p>
-          </s.PerformanceDetail1>
-          <p>
-            <s.PerformanceTitle>
-            {performance.prfStartDt} ~ {performance.prfEndDt}
-            </s.PerformanceTitle>
-            <s.PerformanceDetail2 style={{fontSize: "17.5px"}}>
-            <br />
-            {performance.dtGuidance?.split('),').map((time) => time.trim()).map((line, idx) => (<span key={idx}>{line.endsWith(')') ? line : line + ')'}<br /></span>))}
-            </s.PerformanceDetail2>
-            <s.PerformanceDetail3>
-          <p style={{ fontSize: "17.5px"}}>⏱ {performance.runtime} <br /> 🎭 {performance.genreNm}<br />👶 {performance.prfAge}</p>
-          </s.PerformanceDetail3>
-        <s.PerformanceDetail3>
-          <p style={{ fontSize: "17.5px", margin: 0, lineHeight: 1.5 }}>
-          {performance.child === "Y" && "✔️ 미취학 아동 입장 가능"}</p>
-          <p style={{ fontSize: "17.5px", margin: 0, lineHeight: 1.5 }}>
-          {performance.visit === "Y" && "✔️ 내한 공연"}</p>
-          </s.PerformanceDetail3>
+      <div style={{marginTop: 0,  width: "524px"}}>
+        <s.PerformanceTitle>
+          <h1 style={{marginTop: 0}}>{removeRegionTag(performance.prfNm)}</h1>
+        </s.PerformanceTitle>
         <s.PerformanceDetail1>
-        <p style={{fontSize: "17.5px"}}>
+          <h2 style={{ margin: 0, lineHeight: 2 }}>
+            {(() => {
+              const original = performance.prfPlcNm;
+              let result = '';
+              const seen = new Set<string>();
+              original.split(/\s*(\([^)]+\))/).forEach((part) => {
+                if (!part) return;
+                if (!part.startsWith('(')) {
+                  result += part;
+                  return;
+                }
+                const content = part.slice(1, -1).trim();
+                const normalizedContent = content.replace(/\s+/g, '');
+                const normalizedResult = result.replace(/\s+/g, '');
+                if (seen.has(normalizedContent) || normalizedResult.includes(normalizedContent)) {
+                  return;
+                }
+                seen.add(normalizedContent);
+                result += `(${content})`;
+              });
+              return result.trim();
+            })()}
+          </h2>
+          <p>{performance.area}</p>
+        </s.PerformanceDetail1>
+
+        <s.PerformanceTitle>
+          {performance.prfStartDt} ~ {performance.prfEndDt}
+        </s.PerformanceTitle>
+        <s.PerformanceDetail2 style={{fontSize: "17.5px"}}>
           <br />
-          {performance.ticketPrice?.toString().split(', ').map((price, index) => (
-            <span key={index}>
-              {price}
-              <br />
-            </span>
-          ))}
-      </p>
-      </s.PerformanceDetail1>
-        </p>
+          {performance.dtGuidance?.split('),').map((time) => time.trim()).map((line, idx) => (<span key={idx}>{line.endsWith(')') ? line : line + ')'}<br /></span>))}
+        </s.PerformanceDetail2>
+        <s.PerformanceDetail3>
+        <p style={{ fontSize: "17.5px"}}>⏱ {performance.runtime} <br /> 🎭 {performance.genreNm}<br />👶 {performance.prfAge}</p>
+        </s.PerformanceDetail3>
+        <s.PerformanceDetail3>
+        <p style={{ fontSize: "17.5px", margin: 0, lineHeight: 1.5 }}>
+        {performance.child === "Y" && "✔️ 미취학 아동 입장 가능"}</p>
+        <p style={{ fontSize: "17.5px", margin: 0, lineHeight: 1.5 }}>
+        {performance.visit === "Y" && "✔️ 내한 공연"}</p>
+        </s.PerformanceDetail3>
+        <s.PerformanceDetail1>
+        {performance.ticketPrice?.toString().split(', ').map((price, idx) => (
+          <p key={`price-${idx}`} style={{ fontSize: "17.5px", marginBottom: "5px" }}>
+            {price}
+          </p>
+        ))}
+        </s.PerformanceDetail1>
+
+        <div
+          onMouseEnter={() => setShowLinks(true)}
+          onMouseLeave={() => setShowLinks(false)}
+          style={{display: "flex", flexDirection: "column", alignItems: "center", marginTop: "0px"}}
+        >
         <button
-          onClick={() => {
-            const firstUrl = performance.providerUrl.split(",")[0].trim();
-            const validUrl = firstUrl.startsWith("http")
-              ? firstUrl
-              : `https://${firstUrl}`;
-            window.open(validUrl, "_blank");
-          }}
           style={{
-            padding: "0 30px",
+            width: "200px",
+            height: "60px",
+            marginTop: "20px",
+            backgroundColor: "#Bf1C1C",
+            color: "#dbdbdb",
+            fontWeight: 500,
+            borderRadius: "6px",
+            textAlign: "center",
             lineHeight: "60px",
-            fontSize: "23px",
-            cursor: "pointer",
-            borderRadius: "30px",
-            backgroundColor: "white",
-            color: "black",
+            border: "none",
+            cursor: "pointer"
           }}
         >
-          예매 바로가기→
+          예매 바로가기 →
         </button>
-      <hr style={{ width: "100%", border: "1px solid #ccc", margin: "120px 0", marginBottom: "90px" }} />
+        
+        {showLinks && (
+          <div
+            onMouseEnter={() => setShowLinks(true)}
+            onMouseLeave={() => setShowLinks(false)}
+            style={{
+              marginTop: "10px",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              gap: "10px",
+              transition: "all 0.2s"
+            }}
+          >
+            {performance.providerUrl
+              .split(",")
+              .map((rawUrl, index) => {
+                const trimmed = rawUrl.trim();
+                if (!trimmed) return null;
+
+                const validUrl = trimmed.startsWith("http")
+                  ? trimmed
+                  : `https://${trimmed}`;
+
+                const siteName = getSiteName(validUrl);
+
+                return (
+                  <button
+                    key={index}
+                    onClick={() => window.open(validUrl, "_blank")}
+                    style={{
+                      width: "200px",
+                      height: "50px",
+                      backgroundColor: "#Bf1C1C",
+                      color: "#f0f0f0",
+                      fontWeight: 500,
+                      borderRadius: "6px",
+                      textAlign: "center",
+                      lineHeight: "50px",
+                      display: "block",
+                      border: "none",
+                      cursor: "pointer"
+                    }}
+                  >
+                    {siteName}
+                  </button>
+                );
+              })}
+          </div>
+        )}
+        </div>
+        </div>
+        <HeartIcon style={{position: "absolute", top: "40px", right: "-80px", fontSize: "40px", color: "crimson", cursor: "pointer"}}  onClick={handleToggleFavorite}/>
+      </div>
+      
+      <hr style={{ width: "100%", border: "1px solid #ccc", marginTop: "0px",marginBottom: "50px" }} />
       {performance.detailImgUrl && performance.detailImgUrl.trim() !== "" && (
         <div
           style={{
